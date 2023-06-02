3ccd4cb107ca06a7273eec73c951b73762e70d93
Mazlum TOSUN <mazlum.tosun@gmail.com>
2023-06-02 15:33:51 +0200

    Add a different CI service name for Cloud Build, Dagger and Gitlab CI, in order to check that the Flex Template image and spec file are correctly generated for each service.

--- scripts/build_image_and_create_spec_file_with_template_build.sh
+++ scripts/build_image_and_create_spec_file_with_template_build.sh
@@ -6,8 +6,8 @@ set -u
 
 echo "#######Creating Dataflow Flex Template"
 
-gcloud dataflow flex-template build "$METADATA_TEMPLATE_FILE_PATH" \
-  --image-gcr-path "$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:$IMAGE_TAG" \
+gcloud dataflow flex-template build "$METADATA_TEMPLATE_FILE_PATH-$CI_SERVICE_NAME.json" \
+  --image-gcr-path "$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME/$CI_SERVICE_NAME:$IMAGE_TAG" \
   --sdk-language "$SDK_LANGUAGE" \
   --flex-template-base-image "$FLEX_TEMPLATE_BASE_IMAGE" \
   --metadata-file "$METADATA_FILE" \
